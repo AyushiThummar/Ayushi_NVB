69dc22a429df705f0effbe692ff86ae1d098e87e
Ayushi <Ayushi@AYUSHI>
2025-07-01 17:35:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayushi_NVB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = 35;
+            double temp = 14.25;
+            char gender = 'F';
+            string name;
+            name = "Ayushi";
+            Console.WriteLine("Number is : " + no);
+            Console.WriteLine("Temperature is : " + temp);
+            Console.WriteLine("Gender is : " + gender);
+            Console.WriteLine("Name is : " + name);
+        }
+    }
+}
