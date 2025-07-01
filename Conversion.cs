3e6cea2ebad73e1dc3873cceeeb62975a3b560c8
Ayushi Thummar <ayushithummar14@gmail.com>
2025-07-01 18:00:07 +0530

    Add files via upload

---
+++ Conversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayushi_NVB
+{
+    internal class Conversion
+    {
+        public static void main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter string value : ");
+            str = Console.ReadLine();
+            Console.WriteLine("String is : " + str);
+            int no1;
+            int no2;
+            Console.WriteLine("Enter two numbers no1 and no2 : ");
+            no1 = Convert.ToInt32(Console.ReadLine());
+            no2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("First number is : " + no1);
+            Console.WriteLine("First number is : " + no2);
+        }
+    }
+}
