92b3fcdd60052a9348eb28fec559277246155aab
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-03 18:11:00 +0530

    Saved local edits before pulling

---
+++ First_fifty_even_numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayushi_NVB
+{
+    internal class First_fifty_even_numbers
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayushi_NVB
+{
+    internal class Program1
+    {
+        public static void Main(string[] args)
+        {
+            int no;
+            no = 1;
+            Console.WriteLine("Printing numbers 1 to 10:");
+            while (no <= 10)
+            {
+                Console.WriteLine(no);
+                no++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Printing numbers 10 to 1:");
+            no = 10;
+            while (no >= 1)
+            {
+                Console.WriteLine(no);
+                no--;
+            }
+        }
+    }
+}
