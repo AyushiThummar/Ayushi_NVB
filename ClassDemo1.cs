661af140ea32f85dc18622148d9deca66326aabf
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-29 20:36:29 +0530

    2 program of class added.

---
+++ ClassDemo1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Ayushi_NVB
+{
+    public class Employee
+    {
+        int empId;
+        string eName;
+        double salary;
+        //public void getData()
+        //{
+        //    empId = 111;
+        //    eName = "A";
+        //    salary = 20000.00;
+        //}
+
+        public void getData(int empId, string eName, double salary)
+        {
+            this.empId = empId;
+            this.eName = eName;
+            this.salary = salary;
+        }
+        public double getSalary()
+        {
+            return salary;
+        }
+        public void showData()
+        {
+            Console.WriteLine("Emp ID : "+ empId);
+            Console.WriteLine("Emp Name  : " + eName);
+            Console.WriteLine("Emp Salary : " + salary);
+        }
+    }
+    internal class ClassDemo1
+    {
+        public static void Main(string[] args)
+        {
+            //Employee e1 = new Employee();
+            //e1.getData();
+            //e1.showData();
+            ////e1.eName = "B";   // It will give error
+
+            //Employee e2 = new Employee();
+            //e2.getData();
+            //e2.showData();
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //Employee e1 = new Employee();
+            //e1.getData(111, "AA", 20000.00);
+            //e1.showData();
+
+            //Employee e2 = new Employee();
+            //e2.getData(112, "AB", 25000.00);
+            //e2.showData();
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            Employee e1 = new Employee();
+            e1.getData(111, "AA", 20000.00);
+
+            Employee e2 = new Employee();
+            e2.getData(112, "AB", 25000.00);
+
+            if (e1.getSalary() > e2.getSalary())
+            {
+                e1.showData();
+            }
+            else
+            {
+                e2.showData();
+            }
+        }
+    }
+}
---
+++ ClassDemo2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Ayushi_NVB
+{
+    public class Time
+    {
+        double hour;
+        double minute;
+        double second;
+    public void getTime(double hour, double minute, double second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    public void showTime()
+        {
+            Console.WriteLine("Time : " + hour + ":" +  minute + ":" + second);
+        }
+        public void incrementSecond()
+        {
+            second++;
+
+            if (second == 60)
+            {
+                second = 0;
+                minute++;
+
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+
+                    if (hour == 24)
+                    {
+                        hour = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    internal class ClassDemo2
+    {
+        public static void Main(string []args)
+        {
+            Time t1 = new Time();
+            t1.getTime(11, 28, 59);
+            Console.Write("Current Time => ");
+            t1.showTime();
+
+            t1.incrementSecond();
+            Console.Write("After Increment => ");
+            t1.showTime(); ;
+
+            Time t2 = new Time();
+            t2.getTime(11, 59, 59);
+            Console.Write("Current Time => ");
+            t2.showTime();
+
+            t2.incrementSecond();
+            Console.Write("After Increment => ");
+            t2.showTime();
+        }
+    }
+}
